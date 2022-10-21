471a3995f26692a0bad2cc72926084e85860b934
Miron <alexander.mironov.jr@ya.ru>
2022-10-21 17:49:01 +0300

    update rabbit host

--- core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitConfig.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
-    private static final String HOST = "localhost";
+    private static final String HOST = "rabbit";
 
     @Bean
     public ConnectionFactory connectionFactory() {
